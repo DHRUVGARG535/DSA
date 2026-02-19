a8a67b285180013b45e6ea105edc6a7aa4f64d89
DHRUVGARG535 <157580494+DHRUVGARG535@users.noreply.github.com>
2026-02-19 23:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.26%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.length();
+        int count = 0;
+
+        for(int i =0;i<n;i++) {
+            if(word[i] >= 'A' && word[i] <= 'Z'){
+                count++;
+            }  
+
+
+        }
+         if(count == n || count == 0)
+        return true;
+
+        if(word[0]>='A' && word[0]<='Z' && count ==1)
+        return true;
+
+        return false;
+    }
+};
